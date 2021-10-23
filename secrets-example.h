1ce0e6023ed323a0c3598e037cc3a6fa41f42b66
Bilal <11070669+zeidanbm@users.noreply.github.com>
2021-10-22 20:38:20 -0400

    added secrets example and fixed readme

---
+++ secrets-example.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+#define BROKER ""
+#define PORT 16180
+#define TOPIC "v1/devices/me/telemetry"
+#define IN_TOPIC "v1/devices/me/rpc/request/+"
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
